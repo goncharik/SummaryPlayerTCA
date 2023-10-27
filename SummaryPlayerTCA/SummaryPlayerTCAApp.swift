76b5e6753cdd6f7d5c707286ea738ea188e55d63
Eugene Honcharenko <tulusheg@gmail.com>
2023-10-27 19:16:57 -0400

    Initial Commit

---
+++ SummaryPlayerTCA/SummaryPlayerTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryPlayerTCAApp.swift
+//  SummaryPlayerTCA
+//
+//  Created by Eugene Honcharenko on 2023-10-27.
+//
+
+import SwiftUI
+
+@main
+struct SummaryPlayerTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
